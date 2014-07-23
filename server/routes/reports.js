77fc8f3496048d42a2b755d7226dce3fb34fd3e7
Richard Lee <rblee88@gmail.com>
2014-07-23 18:42:59 -0500

    Can post address information for new reports
    When creating a new Report, you can send latitude, longitude, and
    address as params to be stored as a Location associated to that Report.

--- server/routes/reports.js
+++ server/routes/reports.js
@@ -7,14 +7,45 @@ exports.index = function(req, res){
 };
 
 exports.create = function(req, res){
-  var report = db.Report.build({
-    'recyclingAvailable': req.param('recyclingAvailable')
-  })
+  var lat = req.param('latitude'),
+      lng = req.param('longitude'),
+      address = req.param('address');
 
-  report.save().success(function(savedTask){
-    res.json({'report': savedTask})
-  }).error(function(){
-    res.json({'error': 'Failed to store report'})
+  db.Location.find({
+    'where': {'latitude': lat, 'longitude': lng}
+  }).success(function(location){
+    if(location){
+      var report = db.Report.build({
+        'recyclingAvailable': req.param('recyclingAvailable'),
+        'locationId': location['id']
+      })
+
+      report.save().success(function(savedTask){
+        res.json({'report': savedTask})
+      }).error(function(){
+        res.json({'error': 'Failed to store report'})
+      })
+    } else {
+      var newLocation = db.Location.build({
+        'latitude': lat,
+        'longitude': lng,
+        'address': address
+      })
+      newLocation.save().success(function(location){
+        var report = db.Report.build({
+          'recyclingAvailable': req.param('recyclingAvailable'),
+          'locationId': location['id']
+        })
+
+        report.save().success(function(savedTask){
+          res.json({'report': savedTask})
+        }).error(function(){
+          res.json({'error': 'Failed to store report'})
+        })
+      })
+    }
+  }).error(function(error){
+    console.log('Errored out while finding location with latitude: ' + lat + ' longitude: ' + lng)
   })
 }
 
