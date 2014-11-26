fe15fe5057046ebb5a4b783b39296b2b4ad8d480
Ben Wilhelm <ben@doublebeamdesign.com>
2014-11-25 20:01:09 -0600

    Issue #44 - Add active state to navbar
    Active class to indicate which page you're currently on

--- routes/index.js
+++ routes/index.js
@@ -9,18 +9,26 @@ var express = require('express')
 router.use(function(req, res, next){
   var tplFiles = fs.readdirSync('./public/js/view_handlers');
   res.locals.tplFiles = tplFiles;
+  res.locals.navActive = {
+    home: '',
+    getinvolved: '',
+    about: ''
+  }
   next();
 });
 
 router.get('/', function(req, res){
+  res.locals.navActive.home = 'active';
   res.render('index');
 });
 
 router.get('/about', function(req, res) {
+  res.locals.navActive.about = 'active';
   res.render('about');
 });
 
 router.get('/get-involved', function(req, res) {
+  res.locals.navActive.getinvolved = 'active';
   res.render('getinvolved');
 });
 
