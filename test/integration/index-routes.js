f0b91fd7cf61f63f286f51bce027a5a4289ecfcd
Alex Kahn <alex@sweetride.com>
2015-01-26 12:40:54 -0600

    more integration testing, add cheerio for DOM work

--- test/integration/index-routes.js
+++ test/integration/index-routes.js
@@ -2,15 +2,34 @@ process.env.NODE_ENV = 'test';
 var app = require('../../server')
   , expect = require('chai').expect
   , request = require('request')
+  , cheerio = require('cheerio')
   ;
 
-describe("Integration Tests", function(){
+describe("Integration Tests - Index Route", function(){
+  
   describe('GET /', function(){
-    it("Renders the proper template", function(){})
-    it("Renders the address form", function(){})
-    it("Highlights the proper nav icon", function(){})
-    it("Renders the map", function(){})
+    it("Responds with a 200", function (done) {
+      request.get('http://localhost:3000/', function (err, res, body) {
+        expect(err).to.equal(null);
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+    
+    it("Renders the address form", function (done) {
+      request.get('http://localhost:3000/', function (err, res, body) {
+        expect(err).to.equal(null);
+        $ = cheerio.load(body);
+        console.log($.root().find('#searchForm').length);
+        done();
+      });
+    });
+    
+    it("Highlights the proper nav icon", function (done) {done();});
+    
+    it("Renders the map", function (done) {done();})
   });
+  
   describe('GET /about', function(){});
   describe('GET /get-involved', function(){});
   describe('GET /contact', function(){});
