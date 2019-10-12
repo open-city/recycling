e34a6ff510add1f6a91e36943639452be1573b6d
Alex Kahn <alex.kahn@outlook.com>
2019-10-12 10:38:11 -0500

    debug cors

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ var corsOptions = {
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true)
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error(`${origin} Not allowed by CORS`))
     }
   }
 }
